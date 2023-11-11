665d42fffcf80d2ca9bf8c0a4c299a61c06ab99f
suhana-Mattekhane <144325000+suhana-Mattekhane@users.noreply.github.com>
2023-11-11 18:59:05 +0530

    Example of XOR operator

---
+++ XOR.java
@@ -0,0 +1,9 @@
+public class XOR {
+    // Use of XOR operator
+    public static void main(String args[])
+    {
+        int number1 = 5;
+        int number2 = 4;
+        System.out.println(number1 ^ number2);
+    }
+}
