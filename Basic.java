77d4f20d9cc618818c6fdc08a0d9dc00145330f2
suhana-Mattekhane <144325000+suhana-Mattekhane@users.noreply.github.com>
2023-11-11 18:55:01 +0530

    Basic java examples
    Creating array of String and Character Retriving it and Type casting examples

---
+++ Basic.java
@@ -0,0 +1,27 @@
+public class Basic {
+    public static void main(String[] args) {
+
+      // creating Array of String
+        String name[]={"suhana","sohail", "sujan"};
+
+        //Creating Array of Character
+        char character[]={'s','u','h','a','n','a'};
+
+        //Retriving the name by its index number
+      System.out.println(name[1]);
+
+      //Retriving the name by its index number
+      System.out.println(character[3]);
+
+      //Type cast Byte data Type to Int datatype
+      byte number1 = 10;
+      long number2 = 1000;
+      int output= (int)number1;
+      System.out.println(output);
+      //Type casting long datatype to short datatype
+      short value = (short)(number2);
+      System.out.println(value);
+
+    }
+
+}
