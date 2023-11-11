75ef6e0919f5926d298d8eaf0473860ba36e190d
suhana-Mattekhane <144325000+suhana-Mattekhane@users.noreply.github.com>
2023-11-11 18:56:28 +0530

    Bitwise Manipulation using NOT operator

---
+++ BitManipulation.java
@@ -0,0 +1,8 @@
+public class BitManipulation {
+    //Bitwise Manipulation using NOT operator
+    public static void main(String[] args) {
+        int number = 3;
+
+        System.out.println(-(~number));
+    }
+}
